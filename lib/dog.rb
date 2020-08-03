f73ef22d5d29d85a06e30336453b1f76a7e8533e
Georgette Class-Peters <georgycp@udel.edu>
2020-08-03 02:22:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,25 +1,9 @@
-class Dog
-
-### 3. Define `#sit` in `Dog`
-
-Add an instance method `#sit` to your `Dog` class in `lib/dog.rb` that will puts
-`"The Dog is sitting"`.
-
-
-
-
-
-
-
-
-
 
 class Dog
   def bark
     puts "Woof!"
   end
-  
+  def sit 
+    puts "The Dog is sitting"
 end
- 
-fido = Dog.new
  
